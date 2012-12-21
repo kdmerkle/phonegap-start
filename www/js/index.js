b5e052fde3d036167b33407f0532c0de7ef8ae4f
Kurt Merkle <merkle.kurt@gmail.com>
2012-12-20 20:54:47 -0500

    e

--- www/js/index.js
+++ www/js/index.js
@@ -39,19 +39,4 @@ var app = {
         var completeElem = document.querySelector('#' + id + ' .complete');
         completeElem.className = completeElem.className.split('hide').join('');
     }
-	
-	// onSuccess: Get a snapshot of the current acceleration
-    //
-    function onSuccess(acceleration) {
-        alert('Acceleration X: ' + acceleration.x + '\n' +
-              'Acceleration Y: ' + acceleration.y + '\n' +
-              'Acceleration Z: ' + acceleration.z + '\n' +
-              'Timestamp: '      + acceleration.timestamp + '\n');
-    }
-
-    // onError: Failed to get the acceleration
-    //
-    function onError() {
-        alert('onError!');
-    }
 };
