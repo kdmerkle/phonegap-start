c6594c139f2842b13b2db18128ce5b78855cad33
Kurt Merkle <merkle.kurt@gmail.com>
2012-12-23 21:46:19 -0500

    x

--- www/js/index.js
+++ www/js/index.js
@@ -46,6 +46,21 @@ var app = {
 		};
 		var compassOptions = {frequency: 1000};
 		var watchId = navigator.compass.watchHeading(compassSuccess, compassError, compassOptions);
+		
+		function checkConnection() {
+		    var networkState = navigator.connection.type;
+
+		    var states = {};
+		    states[Connection.UNKNOWN]  = 'Unknown connection';
+		    states[Connection.ETHERNET] = 'Ethernet connection';
+		    states[Connection.WIFI]     = 'WiFi connection';
+		    states[Connection.CELL_2G]  = 'Cell 2G connection';
+		    states[Connection.CELL_3G]  = 'Cell 3G connection';
+		    states[Connection.CELL_4G]  = 'Cell 4G connection';
+		    states[Connection.NONE]     = 'No network connection';
+
+		    return('Connection type: ' + states[networkState]);
+		}
 		pConnectionType.innerText = checkConnection();
     },
     // Update DOM on a Received Event
@@ -59,21 +74,4 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
-	
-	function checkConnection() {
-	    var networkState = navigator.connection.type;
-
-	    var states = {};
-	    states[Connection.UNKNOWN]  = 'Unknown connection';
-	    states[Connection.ETHERNET] = 'Ethernet connection';
-	    states[Connection.WIFI]     = 'WiFi connection';
-	    states[Connection.CELL_2G]  = 'Cell 2G connection';
-	    states[Connection.CELL_3G]  = 'Cell 3G connection';
-	    states[Connection.CELL_4G]  = 'Cell 4G connection';
-	    states[Connection.NONE]     = 'No network connection';
-
-	    return('Connection type: ' + states[networkState]);
-}
-
-checkConnection();
 };
