f5acaafbedb814d3eb71ae98873329665aa3809a
Kurt Merkle <merkle.kurt@gmail.com>
2013-01-08 20:47:07 -0500

    a

--- www/js/index.js
+++ www/js/index.js
@@ -76,12 +76,12 @@ var compassOptions = {frequency: 1000};
 var geolocationSuccess = function(position) {
 	pPosition.innerHTML =   '<li>Latitude: ' + position.coords.latitude + '</li>' 
 						  + '<li>Longitude: '  + position.coords.longitude + '</li>'
-						  + '<li>Altitude: ' + position.coords.altitude  + '</li>'
-						  + '<li>Accuracy: ' + position.coords.accuracy + '</li>'
+						  + '<li>Altitude: ' + position.coords.altitude  + '</li>';
+						 /* + '<li>Accuracy: ' + position.coords.accuracy + '</li>'
 						  + '<li>Altitude Accuracy: NA (Android)' + '</li>'
 						  + '<li>Heading: ' + position.coords.heading + '</li>'
 						  + '<li>Speed: ' + position.coords.speed + '</li>'
-						  + '<li>Timestamp: ' + position.timestamp;
+						  + '<li>Timestamp: ' + position.timestamp;*/
 };
 var geolocationError = function(error){
 	pPosition.innerHTML = '<li>Error:' + error.code + '</li>';
