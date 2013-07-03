ef9c5105dc6e0ad6c40eacde8d874713da85c95e
Kurt Merkle <kdmerkle@yahoo.com>
2013-07-03 08:24:49 -0400

    individual pages

--- www/js/index.js
+++ www/js/index.js
@@ -16,13 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  
-var pCompassHeading;
-var pConnectionType;
-var pPosition;
-var pDevice;
-var pAccel;
-var networkState;
-var states;
 */
 var app = {
     // Application Constructor
@@ -42,12 +35,6 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-//		divAppMain = document.getElementById('appMain');		
-//		pCompassHeading = document.getElementById('compassheading');		
-//		pConnectionType = document.getElementById('connectiontypename');		
-//		pPosition = document.getElementById('position');
-//		pDevice = document.getElementById('deviceName');
-//		pAccel = document.getElementById('accel');
 	},
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -60,65 +47,4 @@ var app = {
     }
 };
 
-function showValues(){
-
-	//compass
-	var compassOptions = {frequency: 1000};		
 	
-	var compassSuccess = function(heading) {		
-		$('#compassheading').html('<li>' + heading.magneticHeading + '</li>');
-	};
-	var compassError = function(error) {
-		alert('Error:' + error.code);
-	};	
-	var watchId1 = navigator.compass.watchHeading(compassSuccess, compassError, compassOptions);	
-		
-
-	//accelerometer
-	var accelOptions = {frequency: 3000};								 
-	function onAccelSuccess(acceleration) {
-		$('#accel').html('<li>X: ' + acceleration.x + '</li>' +
-						   '<li>Y: ' + acceleration.y + '</li>' +
-						   '<li>Z: ' + acceleration.z + '</li>');
-	};
-	function onAccelError() {
-		$('#accel').html('<li>Accel Error</li>');
-	};
-	var watchId2 = navigator.accelerometer.watchAcceleration(onAccelSuccess, onAccelError, accelOptions);	
-	
-	//GPS
-	var geolocationSuccess = function(position) {
-		$('#position').html('<li>Latitude: ' + position.coords.latitude + '</li>' 
-							  + '<li>Longitude: '  + position.coords.longitude + '</li>'
-							  + '<li>Altitude: ' + position.coords.altitude  + '</li>'
-							  + '<li>Accuracy: ' + position.coords.accuracy + '</li>'
-							  + '<li>Altitude Accuracy: NA (Android)' + '</li>'
-							  + '<li>Heading: ' + position.coords.heading + '</li>'
-							  + '<li>Speed: ' + position.coords.speed + '</li>'
-							  + '<li>Timestamp: ' + position.timestamp);
-	};
-	var geolocationError = function(error){
-		$('#position').html('<li>Error:' + error.code + '</li>');
-	};
-	var geolocationOptions = {maximumAge: 3000, timeout: 5000, enableHighAccuracy: true};
-
-	var watchId3 = navigator.geolocation.watchPosition(geolocationSuccess, geolocationError, geolocationOptions);	
-	
-	
-	//Device
-	$('#devicename').html('<li>Device Name: ' + device.name + '</li>' +	'<li>Device Id:' + device.uuid + '</li>');
-	
-	//Network Connection
-	var networkState = navigator.connection.type;
-	var states = {};
-	states[Connection.UNKNOWN]  = 'Unknown connection';
-	states[Connection.ETHERNET] = 'Ethernet connection';
-	states[Connection.WIFI]     = 'WiFi connection';
-	states[Connection.CELL_2G]  = 'Cell 2G connection';
-	states[Connection.CELL_3G]  = 'Cell 3G connection';
-	states[Connection.CELL_4G]  = 'Cell 4G connection';
-	states[Connection.NONE]     = 'No network connection';
-	
-	$('#connectiontypename').html('<li>' + states[networkState] + '</li>');				
-	
-}	
